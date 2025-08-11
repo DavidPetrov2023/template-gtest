88673c4ff07d4870ceea00e084e65bcde4f67c86
David <david@consilia-brno.com>
2025-08-11 10:16:32 +0200

    exti gpio led blinking

--- boards/nucleo-g070rb/stm32g0xx_it.c
+++ boards/nucleo-g070rb/stm32g0xx_it.c
@@ -1,22 +1,20 @@
-// boards/nucleo-g070rb/stm32g0xx_it.c
 #include "stm32g0xx_hal.h"
+#include "main.h"  // LED4_* a extern htim17
 
-/* Minimal exception handlers */
 void NMI_Handler(void) {}
 void HardFault_Handler(void) { while (1) {} }
 void SVC_Handler(void) {}
 void PendSV_Handler(void) {}
+void SysTick_Handler(void) { HAL_IncTick(); }
 
-/* This drives HAL_Delay() time base (1 ms) */
-void SysTick_Handler(void)
+void TIM17_IRQHandler(void)
 {
-    HAL_IncTick();
-    // For some HAL versions you might also add:
-    // HAL_SYSTICK_IRQHandler();
+    HAL_TIM_IRQHandler(&htim17);
 }
 
-/* If you later use the user button on PC13 (EXTI13) */
-void EXTI4_15_IRQHandler(void)
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
+    if (htim->Instance == TIM17) {
+        HAL_GPIO_TogglePin(LED4_GPIO_PORT, LED4_PIN);
+    }
 }
